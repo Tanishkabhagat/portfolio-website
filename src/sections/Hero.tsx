4ff939bcf5f70da1f01dd55232bd63a7272c8e92
Tanishkabhagat <tanishkab95@gmail.com>
2025-02-05 23:13:34 +0530

    hello

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,14 @@
+import memojiImage from "@/assets/images/memoji-computer.png"
+import Image from "next/image";
+export const HeroSection = () => {
+  return <div>
+    <div className="container">
+      <Image src={memojiImage} alt="person peeking from behind laptop"  />
+      <div>
+        <div></div>
+        <div>Available for new projects</div>
+      </div>
+      <h1>building </h1>
+    </div>
+  </div>;
+};
