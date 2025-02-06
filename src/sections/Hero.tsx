47d73daaa25271bdfee750c1cb1a8574b76cec94
Tanishkabhagat <tanishkab95@gmail.com>
2025-02-06 12:20:52 +0530

    dekho dekho

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,32 @@
 import memojiImage from "@/assets/images/memoji-computer.png"
+import ArrowDown from "@/assets/icons/arrow-down.svg"
 import Image from "next/image";
 export const HeroSection = () => {
-  return <div>
+  return <div className="py-32 md:py-48">
     <div className="container">
-      <Image src={memojiImage} alt="person peeking from behind laptop"  />
-      <div>
-        <div></div>
-        <div>Available for new projects</div>
+    <div className="flex flex-col items-center">
+      <Image src={memojiImage} className="size-[100px]" alt="person peeking from behind laptop"  />
+      <div className=" bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+        <div className="bg-green-500 size-2.5 rounded-full"></div>
+        <div className="text-sm font-medium">Available for new projects</div>
       </div>
-      <h1>building </h1>
+    </div>
+    <div className="max-w-lg mx-auto">
+      <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">building exceptional user experiences</h1>
+      <p className="mt-4 text-center text-white/60 md:text-lg">I specialize in transforming designs into functional high performing, web applications. let's discuss your next project
+      </p>
+    </div>
+      <div className="flex flex-col items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Explore My Work</span>
+          <ArrowDown className="size-4"/>
+        </button>
+        <button className="inline-flex items-center border gap-2 border-white bg-white text-gray-800 h-12 rounded-xl px-6
+        ">
+          <span>👋</span>
+          <span className="font-semibold">let's connect</span>
+        </button>
+      </div>
     </div>
   </div>;
 };
